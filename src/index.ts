c7f1f6297ab75fae84bc8d66f1bd2389b4d90ab8
Juhos Ádám <ajuhos@logiscool.com>
2018-06-06 17:08:32 +0200

    Export tags

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './actions';
 export * from './actionCreators';
 export * from './combineClients';
 export * from './middleware';
+
+import { SHARED, CLIENT } from './tags'
+export const tags = { SHARED, CLIENT };
