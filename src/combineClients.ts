e5a96e7a3ac154f332efc5c518a0fd149a0535ab
Juhos Ádám <ajuhos@logiscool.com>
2018-06-07 14:50:50 +0200

    Execute shared actions on every client

--- src/combineClients.ts
+++ src/combineClients.ts
@@ -1,15 +1,18 @@
 import { PRESENT } from './actions';
 import { CLIENT } from './tags';
-import { Map, fromJS } from 'immutable';
 
-const getClient = (clientId: string, state: Map<string, any>) => state.get(clientId);
-const setClient = (clientId: string, client: string, state: Map<string, any>) => state.set(clientId, client);
+const getClient = (clientId: string, state: any[]) => state.find(c => c.id === clientId);
+const setClient = (clientId: string, client: string, state: any[]) => {
+    const newState = state.slice();
+    newState[state.findIndex(c => c.id === clientId)] = client;
+    return newState
+};
 
-export const combineClients = (clientReducer: Function) => (state: any, action: any) => {
-    if(typeof state === 'undefined') return Map();
-    if(action.type === PRESENT) return fromJS(action.payload.state.clients);
+export const combineClients = (clientReducer: Function) => (state: any[], action: any) => {
+    if(typeof state === 'undefined') return [];
+    if(action.type === PRESENT) return action.payload.state.clients;
 
     return (state && action && typeof action[CLIENT] !== 'undefined')
         ? setClient(action[CLIENT], clientReducer(getClient(action[CLIENT], state), action), state)
-        : state
+        : state.map(c => clientReducer(c, state))
 };
