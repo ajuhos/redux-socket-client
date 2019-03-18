be4f788c29383ad6f582b4c9e50c37a5bf49e91b
Juhos Ádám <ajuhos@logiscool.com>
2019-03-18 16:22:39 +0100

    Use a simple object for client list to allow serialization

--- src/combineClients.ts
+++ src/combineClients.ts
@@ -1,42 +1,51 @@
 import { ADD_CLIENT, PRESENT } from './actions';
 import { CLIENT } from './tags';
 
-type ArrayWithMappings<T> = Array<T> & { mappings: { [key: string]: number } };
-
-const getClient = (clientId: string, state: ArrayWithMappings<any>) => state[state.mappings[clientId]];
-const setClient = (clientId: string, client: string, state: ArrayWithMappings<any>): any => {
-    const newState = state.slice();
-    newState[state.mappings[clientId]] = client;
-    return newState
+type ClientSet = {
+    items: any[],
+    mappings: { [key: string]: number }
 };
 
-export const combineClients = (clientReducer: Function) => (state: ArrayWithMappings<any>|undefined, action: any): any[] => {
+const getClient = (clientId: string, state: ClientSet) => state.items[state.mappings[clientId]];
+const setClient = (clientId: string, client: string, state: ClientSet): any => {
+    const items = state.items.slice();
+    items[state.mappings[clientId]] = client;
+    return {
+        items,
+        mappings: state.mappings
+    }
+};
+
+export const combineClients = (clientReducer: Function) => (state: ClientSet|undefined, action: any): ClientSet => {
     if(typeof state === 'undefined') {
-        const newState: any = [];
-        newState.mappings = {};
-        return newState
+        return {
+            items: [],
+            mappings: {}
+        }
     }
 
     if(action.type === PRESENT) {
         const { clients, shared } = action.payload.state;
-        const newState = clients.map(
+        const newState = clients.items.map(
             (client: any) => clientReducer(client, { type: PRESENT, payload: { state: { shared, client } } })
         );
         newState.mappings = {};
-        clients.forEach((client: any, index: number) => newState.mappings[client.id] = index);
+        clients.items.forEach((client: any, index: number) => newState.mappings[client.id] = index);
         return newState;
     }
 
     if(action.type === ADD_CLIENT) {
-        const newState: any = [ ...state, clientReducer({ id: action[CLIENT] }, action) ];
-        newState.mappings = { ...state.mappings, [action[CLIENT]]: newState.length-1 };
-        return newState
+        const items = [ ...state.items, clientReducer({ id: action[CLIENT] }, action) ];
+        return {
+            items,
+            mappings: { ...state.mappings, [action[CLIENT]]: items.length-1 }
+        }
     }
 
-    const newState: ArrayWithMappings<any> = (state && typeof action[CLIENT] !== 'undefined')
+    return (state && typeof action[CLIENT] !== 'undefined')
         ? setClient(action[CLIENT], clientReducer(getClient(action[CLIENT], state), action), state)
-        : state.map(client => clientReducer(client, action));
-
-    newState.mappings = state.mappings;
-    return newState
+        : {
+            items: state.items.map(client => clientReducer(client, action)),
+            mappings: state.mappings
+        }
 };
