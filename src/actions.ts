9286e0adb00333e7da7e45ea90b006ea24402a8e
Juhos Ádám <ajuhos@logiscool.com>
2019-03-23 17:56:04 +0100

    Add actions for clients connecting and disconnecting

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,7 @@
 export const PRESENT = '@@shared/PRESENT';
 export const SKIP = '@@shared/SKIP';
 export const ADD_CLIENT = '@@shared/ADD_CLIENT';
+export const CONNECT_CLIENT = '@@shared/CONNECT_CLIENT';
+export const DISCONNECT_CLIENT = '@@shared/DISCONNECT_CLIENT';
 export const REMOVE_CLIENT = '@@shared/REMOVE_CLIENT';
 export const CONNECT = '@@shared/CONNECT';
